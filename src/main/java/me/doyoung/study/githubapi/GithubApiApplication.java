f7cc21685a7a08adbbb9d54a1f83e54ed58fc661
doyoung0205 <doyoung0205@naver.com>
2020-12-11 01:12:35 +0900

    Initial commit

---
+++ src/main/java/me/doyoung/study/githubapi/GithubApiApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.study.githubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiApplication.class, args);
+    }
+
+}
